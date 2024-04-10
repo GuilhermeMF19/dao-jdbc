7ba199166b6841bb43f2aff52f573e33c7d4fa41
Guilherme <guilherme_riba@hotmail.com>
2024-04-10 17:10:45 -0300

    Implements DepartmentDaoJDBC - insert

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -7,21 +8,25 @@ import model.dao.DepartmentDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-
 public class Program2 {
 	
 	public static void main(String[] args) {
 	
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
-		System.out.println("=== TEST 1: seller findById ===");
+		System.out.println("=== TEST 1: department findById ===");
 		Department department = departmentDao.findById(3);
 		System.out.println(department);
 		
-		System.out.println("\n=== TEST 2: seller findAll ===");
+		System.out.println("\n=== TEST 2: department findAll ===");
 		List<Department> list = departmentDao.findAll();
 		for (Department obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 3: department insert ===");
+		Department newDepartment = new Department(null, "Food");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
 	}
 }
